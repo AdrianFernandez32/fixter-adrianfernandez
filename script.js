abb09c7a6eb9e2090f441e19565ac1f3e215e2e2
Adrian Fernandez <adrianfersa@meta.com>
2023-06-15 16:08:17 -0700

    showed movies

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const apiKey = 'eda4d8abf723d7f2e65cb02f9a0112cf';
 const baseUrl = 'https://api.themoviedb.org/3';
 
+const moviesGrid = document.getElementById('movies-grid');
+const imageUrl = 'https://image.tmdb.org/t/p/w500';
+
 async function fetchMovies(endpoint) {
   try {
     const response = await fetch(endpoint);
@@ -21,61 +24,96 @@ async function getPopularMovies() {
 
   const popularMovies = await fetchMovies(endpoint);
   console.log('Popular Movies:', popularMovies);
+
+  popularMovies.forEach((movie) => {
+    // Create the movie container
+    const movieContainer = document.createElement('div');
+    movieContainer.classList.add('movie');
+
+    // Create the movie poster
+    const image = document.createElement('img');
+    image.src = imageUrl + movie.poster_path;
+    image.alt = 'Movie Poster';
+    image.id - 'movie-poster';
+    movieContainer.appendChild(image);
+
+    // Create the movie title
+    const title = document.createElement('h3');
+    title.classList.add('title');
+    title.textContent = movie.title;
+    title.id = 'movie-title';
+    movieContainer.appendChild(title);
+
+    // Create the votes count
+    const votes = document.createElement('p');
+    votes.classList.add('votes');
+    votes.textContent = `Votes: ${movie.vote_average}`;
+    votes.id = 'movie-votes';
+    movieContainer.appendChild(votes);
+
+    // Append the movie container to the movies grid
+    moviesGrid.appendChild(movieContainer);
+  });
 }
 
-async function getActionMovies() {
-  const endpoint = `${baseUrl}/discover/movie?api_key=${apiKey}&with_genres=28`;
-
-  const actionMovies = await fetchMovies(endpoint);
-  console.log('Action Movies:', actionMovies);
-}
-
-async function getRomanticMovies() {
-  const endpoint = `${baseUrl}/discover/movie?api_key=${apiKey}&with_genres=10749`;
-
-  const romanticMovies = await fetchMovies(endpoint);
-  console.log('Romantic Movies:', romanticMovies);
-}
-
-// async function getPopularMoviesBackgroundImages() {
-//   const endpoint = `${baseUrl}/movie/popular?api_key=${apiKey}`;
-
-//   const popularMovies = await fetchMovies(endpoint);
-
-//   const topFourMovies = popularMovies.slice(0, 4);
-
-//   topFourMovies.forEach((movie) => {
-//     const div = document.createElement('div');
-//     div.style.backgroundImage = `url(https://image.tmdb.org/t/p/original${movie.backdrop_path})`;
-//     div.innerHTML = `
-//       <h2>${movie.title}</h2>
-//       <p>${movie.overview}</p>
-//       <p>Release Date: ${movie.release_date}</p>
-//     `;
-//     div.className = 'carousel-item'; // Add a class name for the carousel item
-//     carouselItemsContainer.appendChild(div);
-//   });
-
-//   // Add event listeners to the navigation buttons
-//   const prevBtn = document.querySelector('.prev-btn');
-//   const nextBtn = document.querySelector('.next-btn');
-
-//   prevBtn.addEventListener('click', () => {
-//     carouselItemsContainer.scrollBy({
-//       left: -300, // Adjust the scroll distance as needed
-//       behavior: 'smooth',
-//     });
-//   });
-
-//   nextBtn.addEventListener('click', () => {
-//     carouselItemsContainer.scrollBy({
-//       left: 300, // Adjust the scroll distance as needed
-//       behavior: 'smooth',
-//     });
-//   });
-// }
-
-// getPopularMoviesBackgroundImages();
 getPopularMovies();
-getActionMovies();
-getRomanticMovies();
+
+const showMoreButton = document.querySelector('.show-more');
+let currentPage = 0;
+const offset = 20;
+
+function handleSearch(event) {
+  if (event.key === 'Enter') {
+    const query = event.target.value;
+    const moviesGrid = document.getElementById('movies-grid');
+
+    // Clear existing content
+    moviesGrid.innerHTML = '';
+
+    // Send the request to search for movies
+    fetch(
+      `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${query}`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        // Process the search results
+        const movies = data.results;
+        movies.forEach((movie) => {
+          // Create the movie container
+          const movieContainer = document.createElement('div');
+          movieContainer.classList.add('movie');
+
+          // Create the movie poster
+          const image = document.createElement('img');
+          image.src = imageUrl + movie.poster_path;
+          image.alt = 'Movie Poster';
+          image.id - 'movie-poster';
+          movieContainer.appendChild(image);
+
+          // Create the movie title
+          const title = document.createElement('h3');
+          title.classList.add('title');
+          title.textContent = movie.title;
+          title.id = 'movie-title';
+          movieContainer.appendChild(title);
+
+          // Create the votes count
+          const votes = document.createElement('p');
+          votes.classList.add('votes');
+          votes.textContent = `Votes: ${movie.vote_average}`;
+          votes.id = 'movie-votes';
+          movieContainer.appendChild(votes);
+
+          // Append the movie container to the movies grid
+          moviesGrid.appendChild(movieContainer);
+        });
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  }
+}
+
+// Add event listener to the search input
+const searchInput = document.getElementById('search-input');
+searchInput.addEventListener('keyup', handleSearch);
